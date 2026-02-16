aa0eed522e7ac9f2c6d20b8e2cb7e2c089ab825d
Felker <nick.felker@wabtec.com>
2026-02-16 15:35:22 -0500

    Added flags to control model training and data visualization. Also refined the model training parameters to get better accuracy.

--- EmoRecAI/EmoRecAI.py
+++ EmoRecAI/EmoRecAI.py
@@ -12,11 +12,12 @@ Based on Udemy course "Modern Artificial Intelligence Masterclass" by Dr. Ryan A
 Revised by: Nick Felker
 
 Creation Date: 2/11/2026
-Last Date Modified: 2/12/2026
+Last Date Modified: 2/16/2026
 
 """
 
 # REQUIRED IMPORTS AND LIBRARIES
+from pyexpat import model
 import pandas as pd
 import numpy as np
 import os
@@ -53,6 +54,13 @@ except ImportError:
 # Define paths for model outputs (outside project directory)
 MODEL_OUTPUT_DIR = r'C:\Users\nick.felker\Downloads\EmoRecAI_Models'
 os.makedirs(MODEL_OUTPUT_DIR, exist_ok=True)
+weights_path = os.path.join(MODEL_OUTPUT_DIR, 'FacialKeyPoints_weights.keras')
+model_json_path = os.path.join(MODEL_OUTPUT_DIR, 'FacialKeyPoints-model.json')
+
+#Flags to visualize data and train the model - set to 0 to skip visualization and training
+visualize_data = 0
+train_model = 0
+
 
 """
 
@@ -88,36 +96,37 @@ facialKey_df['Image'].iloc[0].shape
 Visualize images from the dataframe to confirm expectations and functionality
 
 """
-"""
-#Visualize a random image with its corresponding key points and allow 0 as a valid index
-i = np.random.randint(0, len(facialKey_df))
-plt.figure(figsize=(3, 3))
-plt.imshow(facialKey_df['Image'].iloc[i], cmap='gray')
-for j in range(1, 31, 2):
-    x = facialKey_df.iloc[i, j-1]
-    y = facialKey_df.iloc[i, j]
-    if np.isfinite(x) and np.isfinite(y):
-        plt.plot(x, y, 'rx')
-plt.axis('off')
-plt.show()
 
-#Visualize a grid of 16 images with their corresponding key points
-fig = plt.figure(figsize=(8, 8))
-for idx in range(64):
-    ax = fig.add_subplot(8, 8, idx + 1)
-    # use positional indexing for the image
-    image = facialKey_df['Image'].iloc[idx]
-    ax.imshow(image, cmap='gray')
+if visualize_data == 1:
+    #Visualize a random image with its corresponding key points and allow 0 as a valid index
+    i = np.random.randint(0, len(facialKey_df))
+    plt.figure(figsize=(3, 3))
+    plt.imshow(facialKey_df['Image'].iloc[i], cmap='gray')
     for j in range(1, 31, 2):
-        x = facialKey_df.iloc[idx, j-1]
-        y = facialKey_df.iloc[idx, j]
+        x = facialKey_df.iloc[i, j-1]
+        y = facialKey_df.iloc[i, j]
         if np.isfinite(x) and np.isfinite(y):
-            ax.plot(x, y, 'rx')
-    ax.axis('off')
+            plt.plot(x, y, 'rx')
+    plt.axis('off')
+    plt.show()
 
-plt.tight_layout()
-plt.show()
-"""
+    #Visualize a grid of 16 images with their corresponding key points
+    fig = plt.figure(figsize=(8, 8))
+    for idx in range(64):
+        ax = fig.add_subplot(8, 8, idx + 1)
+        # use positional indexing for the image
+        image = facialKey_df['Image'].iloc[idx]
+        ax.imshow(image, cmap='gray')
+        for j in range(1, 31, 2):
+            x = facialKey_df.iloc[idx, j-1]
+            y = facialKey_df.iloc[idx, j]
+            if np.isfinite(x) and np.isfinite(y):
+                ax.plot(x, y, 'rx')
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
 
 """
 
@@ -143,28 +152,28 @@ for i in range(len(columns)):
     if i%2 == 1:
         facialKey_df_v[columns[i]] = facialKey_df_v[columns[i]].apply(lambda x: 96. - float(x))
 
-"""
-#Show the orignal image and the flipped image with their corresponding key points to confirm functionality
-plt.imshow(facialKey_df['Image'].iloc[0], cmap='gray')
-for j in range(1, 31, 2):
-    x = facialKey_df.iloc[0, j-1]
-    y = facialKey_df.iloc[0, j]
-    if np.isfinite(x) and np.isfinite(y):
-        plt.plot(x, y, 'rx')
-plt.title('Original Image')
-plt.axis('off')
-plt.show()
+if visualize_data == 1:
+    #Show the orignal image and the flipped image with their corresponding key points to confirm functionality
+    plt.imshow(facialKey_df['Image'].iloc[0], cmap='gray')
+    for j in range(1, 31, 2):
+        x = facialKey_df.iloc[0, j-1]
+        y = facialKey_df.iloc[0, j]
+        if np.isfinite(x) and np.isfinite(y):
+            plt.plot(x, y, 'rx')
+    plt.title('Original Image')
+    plt.axis('off')
+    plt.show()
 
-plt.imshow(facialKey_df_h['Image'].iloc[0], cmap='gray')
-for j in range(1, 31, 2):
-    x = facialKey_df_h.iloc[0, j-1]
-    y = facialKey_df_h.iloc[0, j]
-    if np.isfinite(x) and np.isfinite(y):
-        plt.plot(x, y, 'rx')
-plt.title('Horizontally Flipped Image')
-plt.axis('off')
-plt.show()
-"""
+    plt.imshow(facialKey_df_h['Image'].iloc[0], cmap='gray')
+    for j in range(1, 31, 2):
+        x = facialKey_df_h.iloc[0, j-1]
+        y = facialKey_df_h.iloc[0, j]
+        if np.isfinite(x) and np.isfinite(y):
+            plt.plot(x, y, 'rx')
+    plt.title('Horizontally Flipped Image')
+    plt.axis('off')
+    plt.show()
+
 
 #Concat the original dataframe with the flipped dataframe to create an augmented dataset
 facialKey_df_aug = pd.concat([facialKey_df, facialKey_df_h])
@@ -174,22 +183,22 @@ facialKey_df_aug.shape
 #Randomly increase the brightness of the images in the orignal dataframe and concat the new
 #dataframe with the already augmented dataframe to create an larger dataset
 facialKey_df_copy = copy.copy(facialKey_df)
-facialKey_df_copy['Image'] = facialKey_df_copy['Image'].apply(lambda x: np.clip(random.uniform(1.5, 2)*x, 0.0, 255.0))
+facialKey_df_copy['Image'] = facialKey_df_copy['Image'].apply(lambda x: np.clip(random.uniform(1.1, 1.4)*x, 0.0, 255.0))
 facialKey_df_aug = pd.concat([facialKey_df_aug, facialKey_df_copy])
 facialKey_df_aug.shape
 
-"""
-#Show the brightened images to confirm functionality
-plt.imshow(facialKey_df_copy['Image'].iloc[0], cmap='gray')
-for j in range(1, 31, 2):
-    x = facialKey_df_copy.iloc[0, j-1]
-    y = facialKey_df_copy.iloc[0, j]
-    if np.isfinite(x) and np.isfinite(y):
-        plt.plot(x, y, 'rx')
-plt.title('Brightened Image')
-plt.axis('off')
-plt.show()
-"""
+if visualize_data == 1:
+    #Show the brightened images to confirm functionality
+    plt.imshow(facialKey_df_copy['Image'].iloc[0], cmap='gray')
+    for j in range(1, 31, 2):
+        x = facialKey_df_copy.iloc[0, j-1]
+        y = facialKey_df_copy.iloc[0, j]
+        if np.isfinite(x) and np.isfinite(y):
+            plt.plot(x, y, 'rx')
+    plt.title('Brightened Image')
+    plt.axis('off')
+    plt.show()
+
 
 """
 
@@ -306,13 +315,16 @@ X = Activation('relu')(X)
 X = MaxPool2D((3, 3), strides=(2, 2))(X)
 
 #Stage 2
-X = res_block(X, [64, 64, 256], stage=2)
+X = res_block(X, [32, 32, 128], stage=2)
 
 #Stage 3
-X = res_block(X, [128, 128, 512], stage=3)
+X = res_block(X, [64, 64, 256], stage=3)
 
 #Stage 4
-X = res_block(X, [256, 256, 1024], stage=4)
+X = res_block(X, [128, 128, 512], stage=4)
+
+#Stage 5
+X = res_block(X, [256, 256, 1024], stage=5)
 
 #Average Pooling
 X = GlobalAveragePooling2D(name='avg_pool')(X)
@@ -323,27 +335,71 @@ X = Dense(4096, activation='relu')(X)
 X = Dropout(0.2)(X)
 X = Dense(2048, activation='relu')(X)
 X = Dropout(0.1)(X)
+X = Dense(1024, activation='relu')(X)
+X = BatchNormalization()(X)
+X = Dropout(0.2)(X)
+X = Dense(512, activation='relu')(X)
+X = Dropout(0.1)(X)
 X = Dense(30, activation='relu')(X)
 
 #Model instantiation
 model_1_facialKeyPoints = Model(inputs=X_input, outputs=X)
 model_1_facialKeyPoints.summary()
 
-#ADAM optimizer
-print("Compiling the model...")
-adam = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
+if train_model == 1:
+    #ADAM optimizer
+    print("Compiling the model...")
+    adam = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, amsgrad=False)
+    model_1_facialKeyPoints.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
+
+    #Save the best model with least validation loss
+    checkpoint = ModelCheckpoint(filepath=weights_path, verbose=1, save_best_only=True)
+    history = model_1_facialKeyPoints.fit(X_train, y_train, batch_size=32, epochs=100, validation_split=0.25, callbacks=[checkpoint])
+
+    #Save the model architecture and weights to json
+    model_json = model_1_facialKeyPoints.to_json()
+    with open(model_json_path, 'w') as json_file:
+        json_file.write(model_json)
+
+    print(f"Model training complete and saved to: {MODEL_OUTPUT_DIR}")
+
+    if visualize_data == 1:
+        #Get the model keys
+        history.history.keys()
+
+        #Plot the training and validation loss over epochs
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.title('Model Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend(['loss', 'val_loss'], loc='upper right')
+        plt.show()
+
+#Load the best model
+with open(model_json_path, 'r') as json_file:
+    json_savedModel = json_file.read()
+
+model_1_facialKeyPoints = tf.keras.models.model_from_json(json_savedModel)
+model_1_facialKeyPoints.load_weights(weights_path)
+adam = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-8, amsgrad=False)
 model_1_facialKeyPoints.compile(optimizer=adam, loss='mean_squared_error', metrics=['accuracy'])
 
-#Save the best model with least validation loss
-weights_path = os.path.join(MODEL_OUTPUT_DIR, 'FacialKeyPoints_weights.keras')
-checkpoint = ModelCheckpoint(filepath=weights_path, verbose=1, save_best_only=True)
-history = model_1_facialKeyPoints.fit(X_train, y_train, batch_size=32, epochs=1, validation_split=0.05, callbacks=[checkpoint])
+#Evaluate the model on the test set
+result = model_1_facialKeyPoints.evaluate(X_test, y_test)
+print(f"Test Loss: {result[0]}, Test Accuracy: {result[1]}")
 
-#Save the model architecture and weights to json
-model_json = model_1_facialKeyPoints.to_json()
-model_json_path = os.path.join(MODEL_OUTPUT_DIR, 'FacialKeyPoints-model.json')
-with open(model_json_path, 'w') as json_file:
-    json_file.write(model_json)
+#Read the csv for facial expression data
+facialExp_df = pd.read_csv(r'C:\Users\nick.felker\Downloads\Emotion AI Dataset\icml_face_data.csv')
+facialExp_df.head()
+facialExp_df[' pixels'][0]
 
-print(f"Model training complete and saved to: {MODEL_OUTPUT_DIR}")
+#Function to convert pixel values in string format to array format
+def stoa(x):
+    return np.array(x.split(' ')).reshape(48, 48, 1).astype('float32')
+
+#Function to resize the images to 96x96
+def resize(x):
+    img = x.reshape(48, 48)
+    return cv2.resize(img, dsize=(96, 96), interpolation=cv2.INTER_CUBIC)
 
